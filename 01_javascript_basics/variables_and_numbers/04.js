c76139cb132008330a5917a1561517581a1c2f63
Raymond Saade <raymond.saade@gmail.com>
2022-03-14 23:28:26 -0400

    initial commit

---
+++ 01_javascript_basics/variables_and_numbers/04.js
@@ -0,0 +1,13 @@
+let incrementer = 1;
+let start = incrementer;
+let end;
+let difference;
+incrementer += 1;
+incrementer += 1;
+incrementer += 1;
+incrementer++;
+incrementer++;
+end = incrementer;
+difference = end - start;
+console.log(end);
+console.log(difference);
---
+++ README.md
@@ -0,0 +1,1 @@
+Raymond Saade's solutions for Launch School's **JS210** - Fundamentals of Javascript for Programmers
